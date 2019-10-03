0bee50b1573e0a1dbf7315ac03130957cdd82a53
Joshua-Shockley <thelumbashockley@gmail.com>
2019-10-03 13:34:52 -0500

    added more to the server

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const express = require('express');
 // const server = express();
 const server = require('./server.js');
-server.get('/', (req, res) => {
-    res.status(200).json({ Hello: 'node 22' });
-});
+// server.get('/', (req, res) => {
+//     res.status(200).json({ Hello: 'node 22' });
+// });
 
 
 port = process.env.PORT || 4000;
